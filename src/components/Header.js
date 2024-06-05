cccb8ddb16996cbea198e2362929b13ef7a93629
yethan <yethan444@gmail.com>
2024-06-05 13:20:19 +0200

    Updated to Cinemate

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ export const Header = () => {
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <Link to="/" className="flex items-center">
               <img src={Logo} className="mr-2 h-6 sm:h-9" alt="Cinemate Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinema mate</span>
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinemte</span>
           </Link>
 
           <div id="mobile-nav" className="flex md:order-2">
